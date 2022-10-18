9d2aae31c4e0c3293071da2e7dccf7a88eaf1f6e
Alexey.V <alexey.v@phpdev.org>
2022-10-18 23:45:42 +0300

    Added exception for max distances
    Добавлено исключение, при котором нельзя устанавливать свойство первой максимальной дистанции больше, чем значение второй максимальной дистанции. Из-за этого расчёты  велись бы неправильно.

--- Calculator.php
+++ Calculator.php
@@ -50,16 +50,25 @@ class Calculator
      * Метод рассчета стоимости услуг
      * @param int $distance Расстояние в километрах
      * @return int Стоимость услуг в рублях
+     * @throws Exception
      */
     public function calculate(int $distance): int {
-        if ($distance > $this->maxDistanceSecond) {
-            $cost = ($distance - $this->maxDistanceSecond) * $this->priceThird + $this->getDefaultCoastOver300();
-        } else if ($distance > $this->maxDistanceFirst && $distance <= $this->maxDistanceSecond) {
-            $cost = ($distance - $this->maxDistanceFirst) * $this->priceSecond + $this->getDefaultCoastOver100();
-        } else {
-            $cost = $distance * $this->priceFirst;
-        }
+        try {
+            if ($this->maxDistanceFirst > $this->maxDistanceSecond) {
+                throw new Exception('Значение maxDistanceFirst больше, чем maxDistanceSecond !');
+            }
 
-        return $cost;
+            if ($distance > $this->maxDistanceSecond) {
+                $cost = ($distance - $this->maxDistanceSecond) * $this->priceThird + $this->getDefaultCoastOver300();
+            } else if ($distance > $this->maxDistanceFirst && $distance <= $this->maxDistanceSecond) {
+                $cost = ($distance - $this->maxDistanceFirst) * $this->priceSecond + $this->getDefaultCoastOver100();
+            } else {
+                $cost = $distance * $this->priceFirst;
+            }
+
+            return $cost;
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
     }
 }
